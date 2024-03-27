e77d5e2a0060cb82184fc00c53be0e6081503ec0
kayak mmx <kayak_mmx@hotmail.com>
2024-03-26 23:44:39 -0500

    Add project files.

---
+++ BlazorAppTest/Shared/Catastral.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorAppTest.Shared
+{
+    public class Catastral
+    {
+        public string Sector { get; set; } = "0";
+        public string Manzana { get; set; } = "0";
+        public string Predio { get; set; } = "0";
+        public string Division { get; set; } = "0";
+        public string PVertical { get; set; } = "0";
+        public string PHorizontal { get; set; } = "0";
+
+        public override string ToString()
+        {
+            return $"{Sector}-{Manzana}-{Predio}-{Division}-{PVertical}-{PHorizontal}";
+        }
+    }
+}
